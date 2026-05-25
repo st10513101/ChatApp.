7881290f04bbeb2f6866a91ac5d870542c624257
st10513101 <ST10513101@rcconnect.edu.za>
2026-05-25 21:27:18 +0200

    feat Initial commit message.java file

---
+++ src/main/java/com/mycompany/chatapp/JavaMessage.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chatapp;
+
+/**
+ *
+ * @author lihle
+ */
+public class JavaMessage {
+    //Atributes of a single chat message 
+    private String messagePayload;
+    private boolean isSent;
+    private boolean isRecieved;
+    private boolean isRead;
+    
+    //contruscucter to build a message object when its sent
+    public JavaMessage(LString messagePayload, boolean isSent, boolean isRecieved,
+            this.messagePayload= messagePayload;{
+            this.isSent = isSent;
+            this.isRecieved = isReceived;
+            this.isRead = isRead;
+}
+public String getStatusMessage(){
+    return "Status: Message Sent [" + isSent + "], [" +isRecived + ] 
+
+            
+
